44f6b33fb7d049814e08d3230bb7d59830f60666
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-12 21:24:19 -0300

    Fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 python3 setup.py sdist
-python3 -m twine --repository-url https://${NODIS_PYPI_HOST}/simple -u ${NODIS_PYPI_USER} -p ${NODIS_PYPI_PASSWORD} upload dist/*
+python3 -m twine upload --repository-url https://${NODIS_PYPI_HOST}/simple -u ${NODIS_PYPI_USER} -p ${NODIS_PYPI_PASSWORD} dist/*
