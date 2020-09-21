3fb9aff230150495b57faa65e497b86fb2da3731
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-09-21 10:09:46 -0300

    fixed entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 BUILD_CMD="python3 setup.py sdist"
 PUBLISH_CMD="python3 -m twine upload --repository-url https://${NODIS_PYPI_HOST}/simple -u ${NODIS_PYPI_USER} -p ${NODIS_PYPI_PASSWORD} dist/*"
 
-if [[ -z ${NODIS_DEPLOY_ENV} ]]; then
-    pip -i https://${NODIS_PYPI_USER}:${NODIS_PYPI_PASSWORD}@${NODIS_PYPI_HOST}/simple install pyalias
+if [[ -n "${NODIS_DEPLOY_ENV}" ]]; then
+    pip install -i "https://${NODIS_PYPI_USER}:${NODIS_PYPI_PASSWORD}@${NODIS_PYPI_HOST}/simple" pyalias
     curl -X DELETE "https://${NODIS_PYPI_USER}:${NODIS_PYPI_PASSWORD}@${NODIS_PYPI_HOST}/api/package/{${NODIS_PROJECT_NAME}/{${NODIS_PROJECT_NAME}-${NODIS_DEPLOY_ENV}.tar.gz" || true
     ${BUILD_CMD}
     pyalias --environment ${NODIS_DEPLOY_ENV} dist
